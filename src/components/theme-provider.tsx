43ba67247de82409735e047c861a2465c2db24a2
zloe310 <310eee@gmail.com>
2025-09-17 10:14:21 +0300

    Fix ThemeProvider attribute type compatibility

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -5,7 +5,7 @@ import { ThemeProvider as NextThemesProvider } from "next-themes"
 
 interface ThemeProviderProps {
   children: React.ReactNode
-  attribute?: string
+  attribute?: "class" | "data-theme" | "data-mode"
   defaultTheme?: string
   enableSystem?: boolean
   disableTransitionOnChange?: boolean
